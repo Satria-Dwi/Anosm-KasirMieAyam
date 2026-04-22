054f57ab42257989f98c0b1e999fdfeaa513c620
Satria <satriadwi.2410u@gmail.com>
2026-04-22 21:17:31 +0700

    VERSION 1.0

--- script/auth.js
+++ script/auth.js
@@ -1,25 +1,25 @@
 export function checkAuth(allowedRoles = []) {
     const currentUser = JSON.parse(localStorage.getItem("user"));
 
-    // 🔥 ambil base path project
-    const BASE = "/Anoms-Transc-KasirMieAyam";
-
     // ❌ belum login
     if (!currentUser) {
-        window.location.href = BASE + "/";
+        // window.location.href = "/";
+        window.location.href = "/Anoms-Transc-KasirMieAyam/";
         return;
     }
 
     // ❌ tidak punya role
     if (!currentUser.role) {
-        window.location.href = BASE + "/";
+        // window.location.href = "/";
+        window.location.href = "/Anoms-Transc-KasirMieAyam/";
         return;
     }
 
     // ❌ role tidak diizinkan
     if (allowedRoles.length && !allowedRoles.includes(currentUser.role)) {
         alert("Akses ditolak!");
-        window.location.href = BASE + "/views/dashboard/";
+        // window.location.href = "/dashboard.html";
+        window.location.href = "/Anoms-Transc-KasirMieAyam/views/dashboard/";
         return;
     }
 }
