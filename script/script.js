fdbca92f861b225dedbbe5a1edd3156fcf0fc9cc
Satria <satriadwi.2410u@gmail.com>
2026-04-26 19:21:46 +0700

    update daftar menu

--- script/script.js
+++ script/script.js
@@ -92,7 +92,6 @@ function renderMenu(data = menu) {
     const container = document.getElementById("menu-list");
     if (!container) return;
 
-    // Kelompokkan berdasarkan kategori
     const grouped = data.reduce((acc, item, index) => {
         if (!acc[item.kategori]) acc[item.kategori] = [];
         acc[item.kategori].push({ ...item, index });
@@ -105,12 +104,12 @@ function renderMenu(data = menu) {
         html += `
             <div class="menu-section">
                 <h2 class="menu-title">${kategori}</h2>
-                <div class="menu-grid">
+                <div class="menu-row">
         `;
 
         grouped[kategori].forEach(m => {
             html += `
-                <div class="card" onclick='tambah(${m.index})'>
+                <div class="card horizontal-card" onclick='tambah(${m.index})'>
                     <img src="${m.img}" onerror="this.src='https://via.placeholder.com/150'">
                     <h4>${m.nama}</h4>
                     <p>Rp ${formatRupiah(m.harga)}</p>
